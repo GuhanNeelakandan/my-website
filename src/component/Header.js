073245f6129f74e8cfb9c94bd9d71e46ee0a9514
Guhan <you@example.com>
2022-06-05 18:02:59 +0530

    changes

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Type from "react-typed"
 import Animated from '../Animated';
 import Resume from "../guhan.pdf"
 import Download from "../download.gif"
@@ -12,12 +11,12 @@ function Header() {
         <div className="main-info">
           <h3>Hello, I am</h3>
           <h1><strong>GUHAN N S</strong></h1>
-          <Type
+          {/* <Type
             className="type-string"
             strings={["Full Stack Developer", "photoshop Editor"]}
             typeSpeed={60}
             backSpeed={60}
-            loop />
+            loop /> */}
           <a href={Resume} target="_blank" className="btn-main-offer">Get Resume <img className='icon' src={Download} style={{width:"20px", height:"20px"}}/> </a>
           <Link class="btn btn-dark  mt-4 btns" to={"/about"} role="button">About me  <i class="fa fa-arrow-right" aria-hidden="true"></i></Link>
         </div>
