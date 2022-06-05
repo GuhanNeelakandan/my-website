104d636eee07729347df33132b63772a79001ecf
Guhan <you@example.com>
2022-06-05 18:13:26 +0530

    change

--- src/component/Header.js
+++ src/component/Header.js
@@ -11,12 +11,9 @@ function Header() {
         <div className="main-info">
           <h3>Hello, I am</h3>
           <h1><strong>GUHAN N S</strong></h1>
-          {/* <Type
-            className="type-string"
-            strings={["Full Stack Developer", "photoshop Editor"]}
-            typeSpeed={60}
-            backSpeed={60}
-            loop /> */}
+          <div className='typewriter mt-4'>
+          <h1>FULL STACK DEVELOPER.</h1>
+          </div>
           <a href={Resume} target="_blank" className="btn-main-offer">Get Resume <img className='icon' src={Download} style={{width:"20px", height:"20px"}}/> </a>
           <Link class="btn btn-dark  mt-4 btns" to={"/about"} role="button">About me  <i class="fa fa-arrow-right" aria-hidden="true"></i></Link>
         </div>
